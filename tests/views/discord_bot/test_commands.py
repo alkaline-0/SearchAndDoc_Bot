f4c0908511f9a960f9169a972d6766091394c859
Menna Tullah Magdy Taha <menna.tullah.magdy731@gmail.com>
2025-03-22 15:05:25 +0100

    UPD: added tests to verify job creation

--- tests/views/discord_bot/test_commands.py
+++ tests/views/discord_bot/test_commands.py
@@ -1,13 +1,15 @@
 import discord.ext.test as dpytest
 import pytest
 
-import models.channel
+# flake8: noqa
+import views.discord_bot.commands  # pylint: disable=unused-import
+from background_tasks.thread_worker import job_queue
 
 
 @pytest.mark.asyncio
 async def test_throw_error_channel_does_not_exist(bot):
     await dpytest.message(
-        content="!document_with_LLM wrong_channel test_keyword",
+        content="!document_with_LLM 123453 test_keyword",
         channel=dpytest.get_config().channels[0],
         member=dpytest.get_config().members[0],
     )
@@ -16,16 +18,41 @@ async def test_throw_error_channel_does_not_exist(bot):
 
 
 @pytest.mark.asyncio
-async def test_messages_get_populated(bot):
-    _channel = channel = dpytest.get_config().channels[1]
+async def test_new_job_added_to_queue_when_discord_command_triggered(bot):
+    _channel = dpytest.get_config().channels[0]
+
     await dpytest.message(
         content="hello", channel=_channel, member=dpytest.get_config().members[0]
     )
     await dpytest.message(
         content="test", channel=_channel, member=dpytest.get_config().members[1]
     )
-    await dpytest.message("!document_with_LLM general test_keyword")
 
-    channel = models.channel.Channel(_channel)
-    msgs = await channel.get_channel_messages()
-    assert len(msgs) == 2
+    await dpytest.message("!document_with_LLM %d test_keyword" % (_channel.id))
+
+    job = job_queue.get()
+
+    assert job is not None
+    job_queue.task_done()
+
+
+@pytest.mark.asyncio
+async def test_get_channel_messages_successfully(bot):
+    _channel = dpytest.get_config().channels[0]
+
+    first_msg = await dpytest.message(
+        content="hello", channel=_channel, member=dpytest.get_config().members[0]
+    )
+    second_msg = await dpytest.message(
+        content="test", channel=_channel, member=dpytest.get_config().members[1]
+    )
+
+    command_trigger = await dpytest.message(
+        "!document_with_LLM %d test_keyword" % (_channel.id)
+    )
+
+    job = job_queue.get()
+    job_content = job.channel.get_messages()
+
+    assert job_content == [first_msg, second_msg, command_trigger]
+    job_queue.task_done()
